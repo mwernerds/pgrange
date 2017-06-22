feb5422a2de1d22f7d44d1bb545a109f4de42012
TheUchi <florian.politz@yahoo.com>
2017-06-22 14:25:16 +0200

    added example and changed version to 1.1

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ module1 = Extension('pgrange',
                     extra_compile_args=['-std=c++11','-I/usr/include/postgresql'],
                     libraries=['pq'])
 
-
+# example for extra_compile_args=['-std=c++11','-I/usr/lib/postgresql/9.5/bin','-I/usr/include/postgresql']
 
 setup (name = 'pgrange',
-       version = '1.0',
+       version = '1.1',
        description = 'This is a tool for fast range queries for Deep Learning from LiDAR cells',
        ext_modules = [module1])
